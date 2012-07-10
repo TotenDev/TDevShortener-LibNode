60d1f505f70bf2397e0bbc01a7f39c64fecbffcf
Gabriel Pacheco <gabriel@totendev.com.br>
2012-07-10 18:56:04 -0300

    iniitial

---
+++ TDevShortener.js
@@ -0,0 +1,31 @@
+/*Modules*/
+var http = require('http');
+var TDShortenerDomain = "www.tdev.mobi";
+
+/* Shortener UltilityClass */
+module.exports = function (address,callback) { return new TDevShortner(address,callback); }
+//Constructor
+var TDevShortener = function(address, callback) { TDevShortener.shortIt(address,function(data){ callback(data); }); }
+
+/**
+ *	run
+ **/
+TDevShortener.prototype.shortIt = function shortIt(url,callback){
+	var options = {
+		'host'		: TDShortenerDomain,
+		'port'		: 80,
+		'path'		: "/create/",
+		'method'	: "POST"
+	};
+	var req = http.request( options, function(res){
+		res.setEncoding('utf8');
+		res.on('data', function(data){ 
+			if (data) callback(true,data); 
+			else callback(false,null);
+		});
+		res.on('error', function(err){ console.log("shorting error: " + err); callback(false,null); });
+	} );
+	req.on('error', function(err){  console.log("shorting error: " + err); callback(false,null); });
+	req.write(url);
+	req.end();
+}
